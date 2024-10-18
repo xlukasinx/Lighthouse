c97ef09c2ff89a496246825180902d81e75fe955
xlukasinx <154477270+xlukasinx@users.noreply.github.com>
2024-10-18 14:00:46 +0200

    Update

--- LightHouse/Assets/Scripts/MouseRotation.cs
+++ LightHouse/Assets/Scripts/MouseRotation.cs
@@ -5,28 +5,41 @@ using UnityEngine;
 public class MouseRotation : MonoBehaviour
 {
 
-    public GameObject camera;
+    public new Camera camera;
     public GameObject lighthouse;
 
+    
+
+     
+    
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+      
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-
+      // Debug.Log("Screen Width : " + Screen.width);
+      
+      if(Input.GetKeyDown(KeyCode.Space))
+      {
+         Debug.Log("Mouse position: " + Input.mousePosition.x);
+          Debug.Log("Screen Width : " + Screen.width);
+      }
+     
        
-        if(Input.GetAxis("Mouse X") < 0) {
-            print("Mouse moved left");
+        if(Input.GetAxis("Mouse X") < 0 && Input.mousePosition.x == 0) {
+          
             camera.transform.RotateAround(lighthouse.transform.position, Vector3.up, 50 * Time.deltaTime);
 
         }
-        if(Input.GetAxis("Mouse X")>0){
+        if(Input.GetAxis("Mouse X")>0 && Input.mousePosition.x == Screen.width - 1){
    
-        print("Mouse moved right");
+        
          camera.transform.RotateAround(lighthouse.transform.position, Vector3.up, -50 * Time.deltaTime);
         }
         
